32d7fff11bcae448ad3510979af16d52941cd2de
Leon <117457646+LeonMT1@users.noreply.github.com>
2026-02-21 03:40:06 +0100

    Added CA5 Docs

--- source.config.ts
+++ source.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
 import { metaSchema, pageSchema } from 'fumadocs-core/source/schema';
 
+export const ca5Docs = defineDocs({
+  dir: 'content/ca5',
+  docs: {
+    schema: pageSchema,
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
+
+export const ca6Docs = defineDocs({
+  dir: 'content/ca6',
+  docs: {
+    schema: pageSchema,
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
+
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections
 export const docs = defineDocs({
